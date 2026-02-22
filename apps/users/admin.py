0fb2bfaa5d1408142de433ed9ef6408f66793fe2
TheDivyanshShukla <shukladivyansh953@gmail.com>
2026-02-23 02:58:12 +0530

    rebrand: CampusPrep

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,7 +5,7 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        ('RGPV Live Details', {'fields': ('phone_number', 'preferred_branch', 'preferred_semester', 'active_subscription_valid_until')}),
+        ('CampusPrep Details', {'fields': ('phone_number', 'preferred_branch', 'preferred_semester', 'active_subscription_valid_until')}),
     )
     list_display = ('username', 'email', 'phone_number', 'preferred_branch', 'preferred_semester', 'is_staff')
     list_filter = UserAdmin.list_filter + ('preferred_branch', 'preferred_semester')
